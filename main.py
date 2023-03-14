e95a86e7a221db89885b068fb62a59ea1f6b90ef
AdiP <84515343+Aditya-Prasad12343@users.noreply.github.com>
2023-03-14 16:29:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,180 +1,69 @@
 import streamlit as st
-# Core Pkgs
+import PyPDF2
+import docx2txt
 
-# EDA Pkgs
-import pandas as pd
-import numpy as np
+# Function to extract text from a PDF file
+def extract_pdf(file_path):
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfFileReader(file)
+        text = ''
+        for page in reader.pages:
+            text += page.extract_text()
+        return text
 
-# Data Viz Pkg
-import matplotlib.pyplot as plt
-import matplotlib
+# Function to extract text from a Word document
+def extract_docx(file_path):
+    return docx2txt.process(file_path)
 
+# Function to convert the extracted text to LaTeX format
+def to_latex(text):
+    # Replace special characters with LaTeX equivalents
+    text = text.replace('&', '\\&')
+    text = text.replace('%', '\\%')
+    text = text.replace('$', '\\$')
+    text = text.replace('#', '\\#')
+    text = text.replace('_', '\\_')
+    text = text.replace('{', '\\{')
+    text = text.replace('}', '\\}')
+    text = text.replace('~', '\\textasciitilde{}')
+    text = text.replace('^', '\\textasciicircum{}')
 
-matplotlib.use("Agg")
-import seaborn as sns
+    # Replace line breaks with LaTeX newline characters
+    text = text.replace('\n', '\\\\')
 
+    # Wrap text in LaTeX document tags
+    latex = '\\documentclass{article}\n\\begin{document}\n'
+    latex += text
+    latex += '\n\\end{document}\n'
 
+    return latex
 
-from st_aggrid import AgGrid
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-from st_aggrid.shared import JsCode
-from download import download_button
-from st_aggrid import GridUpdateMode, DataReturnMode
+# Streamlit app
+def app():
+    st.title('PDF/Word to LaTeX Converter')
 
+    # File upload widget
+    file_path = st.text_input('Enter the file path of a PDF or DOCX file:')
+    file_type = file_path.split('.')[-1].lower()
 
+    # Convert file on button press
+    if st.button('Convert'):
+        if file_path:
+            # Extract text from file
+            if file_type == 'pdf':
+                text = extract_pdf(file_path)
+            elif file_type == 'docx':
+                text = extract_docx(file_path)
+            else:
+                st.error('Unsupported file type. Please upload a PDF or DOCX file.')
 
-def _max_width_():
-    max_width_str = f"max-width: 1800px;"
-    st.markdown(
-        f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
-        unsafe_allow_html=True,
-    )
+            # Convert text to LaTeX format
+            latex = to_latex(text)
 
+            # Display LaTeX code
+            st.code(latex, language='latex')
+        else:
+            st.error('Please enter a file path.')
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
-activities = ["EDA", "Plots","Handle NULL Values", "Graph Prediction"]
-choice = st.sidebar.selectbox("Select Activities", activities)
-
-if choice == 'EDA':
-    st.title("Exploratory Data Analysis")
-    st.subheader("A Final Year Project")
-    st.text("By Dishti Kundra and Aditya Prasad")
-
-    data = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
-    st.info(
-            f"""
-                    Sample File: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
-                    """
-    )
-
-    if data is not None:
-        df = pd.read_csv(data)
-        st.dataframe(df.head())
-
-
-        if st.checkbox("Export data"):
-            from st_aggrid import GridUpdateMode, DataReturnMode
-
-            gb = GridOptionsBuilder.from_dataframe(df)
-            gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
-            gb.configure_selection(selection_mode="multiple", use_checkbox=True)
-            gb.configure_side_bar()
-            gridOptions = gb.build()
-
-            st.success(
-                f"""
-                    💡 Tip! Hold the shift key when selecting rows to select multiple rows at once!
-                    """
-            )
-
-            response = AgGrid(
-                df,
-                gridOptions=gridOptions,
-                enable_enterprise_modules=True,
-                update_mode=GridUpdateMode.MODEL_CHANGED,
-                data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-                fit_columns_on_grid_load=False,
-            )
-
-            df = pd.DataFrame(response["selected_rows"])
-
-            st.subheader("Filtered data will appear below 👇 ")
-            st.text("")
-
-            st.table(df)
-
-            st.text("")
-
-            c29, c30, c31 = st.columns([1, 1, 2])
-
-
-            CSVButton = download_button(
-                df,
-                "File.csv",
-                "Download to CSV")
-
-
-
-        if st.checkbox("Show Shape"):
-            st.write(df.shape)
-
-        if st.checkbox("Show Columns"):
-            all_columns = df.columns.to_list()
-            st.write(all_columns)
-
-        if st.checkbox("Summary"):
-            st.write(df.describe())
-
-        if st.checkbox("Show Selected Columns"):
-            selected_columns = st.multiselect("Select Columns", all_columns)
-            new_df = df[selected_columns]
-            st.dataframe(new_df)
-
-        if st.checkbox("Show Value Counts"):
-            st.write(df.iloc[:, -1].value_counts())
-
-        if st.checkbox("Correlation Plot(Matplotlib)"):
-            plt.matshow(df.corr())
-            st.pyplot()
-
-        if st.checkbox("Correlation Plot(Seaborn)"):
-            st.write(sns.heatmap(df.corr(), annot=True))
-            st.pyplot()
-
-        if st.checkbox("Pie Plot"):
-            all_columns = df.columns.to_list()
-            column_to_plot = st.selectbox("Select 1 Column", all_columns)
-            pie_plot = df[column_to_plot].value_counts().plot.pie(autopct="%1.1f%%")
-            st.write(pie_plot)
-            st.pyplot()
-
-
-
-elif choice == 'Plots':
-    st.subheader("Data Visualization")
-    data = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx"])
-    if data is not None:
-        df = pd.read_csv(data)
-        st.dataframe(df.head())
-
-        if st.checkbox("Show Value Counts"):
-            st.write(df.iloc[:, -1].value_counts().plot(kind='bar'))
-            st.pyplot()
-
-        # Customizable Plot
-
-        all_columns_names = df.columns.tolist()
-        type_of_plot = st.selectbox("Select Type of Plot", ["area", "bar", "line", "hist", "box", "kde"])
-        selected_columns_names = st.multiselect("Select Columns To Plot", all_columns_names)
-
-        if st.button("Generate Plot"):
-            st.success("Generating Customizable Plot of {} for {}".format(type_of_plot, selected_columns_names))
-
-            # Plot By Streamlit
-            if type_of_plot == 'area':
-                cust_data = df[selected_columns_names]
-                st.area_chart(cust_data)
-
-            elif type_of_plot == 'bar':
-                cust_data = df[selected_columns_names]
-                st.bar_chart(cust_data)
-
-
-
-            elif type_of_plot == 'line':
-                cust_data = df[selected_columns_names]
-                st.line_chart(cust_data)
-
-            # Custom Plot
-            elif type_of_plot:
-                cust_plot = df[selected_columns_names].plot(kind=type_of_plot)
-                st.write(cust_plot)
-                st.pyplot()
-
+if __name__ == '__main__':
+    app()
